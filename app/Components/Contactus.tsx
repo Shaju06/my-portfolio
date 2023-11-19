60a52736c9dfd7a2e735393f29fdaa1337fb7ad5
Varinder Pal Singh <varinderpalsingh@Varinders-MacBook-Air.local>
2023-11-19 13:45:16 +0530

    Add projects and contact us section

---
+++ app/Components/Contactus.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const ContactUs = () => {
+
+    return (
+        <section id='contact-us' className='text-center mt-6  min-h-[500px]'>
+        <h1 className='text-[24px] border-b-4 border-blue-600 inline-block pb-1 mb-20'>Contact us</h1>
+        <div className='text-[48px]'>
+            Coming Soon
+        </div>
+    </section>
+    )
+}
+
+export default ContactUs
